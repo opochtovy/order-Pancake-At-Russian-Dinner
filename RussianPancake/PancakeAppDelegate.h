22f47aa556576cf2f58695d63b0c86b112db3712
Oleg Pochtovy <opochtovy@yahoo.com>
2014-06-20 14:06:05 +0300

    Initial Commit

---
+++ RussianPancake/PancakeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PancakeAppDelegate.h
+//  RussianPancake
+//
+//  Created by Oleg Pochtovy on 19.06.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PancakeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RussianPancake/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RussianPancake
+//
+//  Created by Oleg Pochtovy on 19.06.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PancakeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PancakeAppDelegate class]));
+    }
+}
